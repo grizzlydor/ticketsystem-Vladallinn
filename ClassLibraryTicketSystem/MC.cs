607ba1c952795f204b8896a1a90f1f1abafb392a
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-09-22 12:25:43 +0200

    Task 3

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class MC
+    {
+        public string Licenseplate { get; set; }
+        public DateTime date { get; set; }
+
+        public double Price()
+        {
+            return 125.0;
+        }
+
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/MCTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        /// <summary>
+        /// The method Price is returning fixed price. The actualValue is 125.0.
+        /// </summary>
+        [TestMethod()]
+        public void PriceTest()
+        {
+            var obj = new MC();
+
+            var actualValue = obj.Price();
+
+            Assert.AreEqual(125.0, actualValue);
+        }
+
+        /// <summary>
+        /// The method VehicleType is returning string. The actualValue is "MC".
+        /// </summary>
+        [TestMethod()]
+        public void VehicleTest()
+        {
+            var obj = new MC();
+
+            var actualValue = obj.Vehicle();
+
+            Assert.AreEqual("MC", actualValue);
+        }
+    }
+}
